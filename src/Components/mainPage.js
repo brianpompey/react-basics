6a32c03657439b20d38777696fa2c9cd807d31b4
brianpompey <brianthescholar@gmail.com>
2021-10-03 16:35:25 -0400

    Added count

--- src/Components/mainPage.js
+++ src/Components/mainPage.js
@@ -2,13 +2,23 @@ import { Component } from 'react';
 import Button from './button';
 import './mainPage.css'
 
+const initialState = {
+    count: 0
+};
 
 class MainPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            ...initialState
+        }
+    }
+
     render() {
         return(
             <div className="main">
                 <div class="count">
-                    
+                    {this.state.count}
                 </div>
 
             
